b2b1380f39a7f7eb1a2ad6160c419e66495db4fa
Mina Bastawros <mina.bastawros@thetrainline.com>
2021-12-01 13:29:15 +0000

    Day 1 large

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -21,7 +21,28 @@ public class Day1 {
 
         System.out.println(numberOfIncreases);
     }
-    
+
+    public void largeSol() {
+        List<Integer> list = getInput();
+
+        List<Integer> sumList = new ArrayList<>();
+
+        for(int i=2;i<list.size();i++) {
+            sumList.add(list.get(i-2)+list.get(i-1)+list.get(i));
+        }
+
+        int numberOfIncreases = 0;
+
+        // First item doesn't count, so we are starting from index 1
+        for(int i=1;i<sumList.size();i++) {
+            if(sumList.get(i) > sumList.get(i-1)) {
+                numberOfIncreases++;
+            }
+        }
+
+        System.out.println(numberOfIncreases);
+    }
+
     List<Integer> getInput() {
         String input = FilesUtil.getContentOf("src/day1/input");
         return Arrays.stream(input.split("\n")).map(Integer::valueOf).toList();
