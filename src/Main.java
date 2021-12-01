cac8c70e26d1c690508ac0962c2469292dea2594
Mina Bastawros <mina.bastawros@thetrainline.com>
2021-12-01 00:37:51 +0000

    init project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import day1.Day1;
+
+public class Main {
+    public static void main(String[] args) {
+        Day1 sol = new Day1();
+        sol.smallSol();
+    }
+}
---
+++ src/day1/Day1.java
@@ -0,0 +1,10 @@
+package day1;
+
+import utils.FilesUtil;
+
+public class Day1 {
+    public void smallSol() {
+        String input = FilesUtil.getContentOf("src/day1/input");
+        System.out.println(input);
+    }
+}
